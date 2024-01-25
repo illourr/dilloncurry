29d689c66fb48e5661a1b145d16b1e706d7816b3
Dillon Curry <dilloncurry@gmail.com>
2024-01-25 14:12:20 -0500

    remove server from astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,9 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import node from "@astrojs/node";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  server: {
-    port: 3000,
-  },
-  output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
 });
