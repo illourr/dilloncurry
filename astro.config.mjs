266cf3b43d6503a47e9879de96f6f47d12fddc91
Dillon Curry <dilloncurry@gmail.com>
2024-01-25 19:45:36 -0500

    Try astro hybrid with vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
+  output: "hybrid",
   adapter: vercel({
     webAnalytics: {
       enabled: true,
