da16531254220be50d1d318e86bc348e095cb531
Dillon Curry <dilloncurry@gmail.com>
2024-01-25 20:09:12 -0500

    Remove image service since I dont have one

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,10 +8,6 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [react(), tailwind()],
   adapter: vercel({
-    imagesConfig: {
-      sizes: [160, 320, 640, 1280],
-    },
-    imageService: true,
     webAnalytics: {
       enabled: true,
     },
