5ea88d57eae59f067172752df0e49ed35b1badd4
Dillon Curry <dilloncurry@gmail.com>
2024-01-25 19:48:30 -0500

    Try server mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  output: "hybrid",
+  output: "server",
   adapter: vercel({
     webAnalytics: {
       enabled: true,
