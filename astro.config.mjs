aa40929beb8512b0dcef530e27e93dd08d7d6b59
Dillon Curry <dilloncurry@gmail.com>
2024-01-25 19:39:50 -0500

    Add vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/serverless";
 import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
@@ -6,4 +7,9 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
