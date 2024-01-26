3354236bd59efe43f8f0c6ad41b41dd368f61d21
Dillon Curry <dilloncurry@gmail.com>
2024-01-25 20:16:27 -0500

    Try setting output

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
+  output: "static",
   adapter: vercel({
     webAnalytics: {
       enabled: true,
